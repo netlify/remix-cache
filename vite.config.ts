3903d3fe2072f4da0c1c47e76015076a90ba6845
Rob Stanford <me@robstanford.com>
2024-02-29 16:21:07 +0000

    feat: swap in-memory to blobs cache
    Co-authored-by: Michal Piechowiak <misiek.piechowiak@gmail.com>
    Co-authored-by: Marc Littlemore <MarcL@users.noreply.github.com>
    Co-authored-by: Matt Kane <ascorbic@users.noreply.github.com>
    Co-authored-by: Tatyana  <taty2010@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 import { vitePlugin as remix } from "@remix-run/dev";
+import slugify from "@sindresorhus/slugify";
+import { mkdir, writeFile } from "node:fs/promises";
+import { join } from "node:path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
+import contactData from "./app/contact-data.json" assert { type: "json" };
 
 export default defineConfig({
-  plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+  plugins: [
+    remix({
+      buildEnd: async () => {
+        console.log("build end");
+        // do stuff
+        const blobDeployDir = join(".netlify", "blobs", "deploy");
+        await mkdir(blobDeployDir, { recursive: true });
+        for (const contact of contactData.contacts) {
+          const id = slugify(`${contact.first}-${contact.last}`);
+          await writeFile(
+            join(blobDeployDir, id),
+            JSON.stringify({ id, ...contact })
+          );
+        }
+      },
+    }),
+    netlifyPlugin(),
+    tsconfigPaths(),
+  ],
 });
